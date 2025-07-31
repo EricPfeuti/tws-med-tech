2ed3326972763bfaaf29690942e1ddaebddd8299
EricPfeuti <eric.pfeuti2007@gmail.com>
2025-07-30 23:46:02 -0300

    Telas de Login Finalizadas + Lógica de Cadastro de Usuários no Banco de Dados - 30/07

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const { MongoClient, ObjectId } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const methodOverride = require('method-override');
 
 const app = express();
 const port = 3001;
@@ -7,6 +10,15 @@ const port = 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'segredo-super-secreto',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+const url = 'mongodb://127.0.0.1:27017/';
+const TWSMedTech = 'TWSMedTech';
 
 // PÁGINA INDEX
 app.get('/', (req, res) => {
@@ -23,6 +35,36 @@ app.get('/cadastroMedico', (req, res) => {
     res.sendFile(__dirname + '/pages/cadastroMedico.html');
 });
 
+// CADASTRO MÉDICO
+app.post('/cadastroMedico', async (req, res) => {
+    const client = new MongoClient(url, { useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+        const banco = client.db(TWSMedTech);
+        const collectionMedicos = banco.collection('medicos');
+
+        const medicoExistente = await collectionMedicos.findOne({ nomeMedico: req.body.nomeMedico });
+
+        if (medicoExistente) {
+            res.send('Aluno já existe! Tente outro nome de usuário.');
+        } else {
+            const senhaCriptografada = await bcrypt.hash(req.body.senhaMedico, 10);
+
+            await collectionMedicos.insertOne({
+                nomeMedico: req.body.nomeMedico,
+                senhaMedico: senhaCriptografada
+            });
+
+            res.redirect('/loginMedico');
+        }
+    } catch (erro){
+        res.send('Erro ao registrar médico.');
+    } finally {
+        client.close();
+    }
+});
+
 // PÁGINA CADASTRO PACIENTE
 app.get('/cadastroPaciente', (req, res) => {
     res.sendFile(__dirname + '/pages/cadastroPaciente.html');
@@ -33,9 +75,38 @@ app.get('/loginMedico', (req, res) => {
     res.sendFile(__dirname + '/pages/loginMedico.html');
 });
 
+// LOGIN MÉDICO
+app.post('/loginMedico', async (req, res) => {
+    const client = new MongoClient(url, { useUnifiedTopology: true });
+
+    try{
+        await client.connect();
+        const banco = client.db(TWSMedTech);
+        const collectionMedicos = banco.collection('medicos');
+
+        const medico = await collectionMedicos.findOne({ nomeMedico: req.body.nomeMedico });
+
+        if (medico && await bcrypt.compare(req.body.senhaMedico, medico.senhaMedico)){
+            req.session.nomeMedico = req.body.nomeMedico;
+            res.redirect('/medico');
+        } else {
+            res.redirect('/erro');
+        }
+    } catch (erro) {
+        res.send('Erro ao realizar login.');
+    } finally {
+        client.close();
+    }
+});
+
 // PÁGINA LOGIN PACIENTE
 app.get('/loginPaciente', (req, res) => {
     res.sendFile(__dirname + '/pages/loginPaciente.html');
+});
+
+// PÁGINA LOGIN PACIENTE
+app.get('/medico', (req, res) => {
+    res.sendFile(__dirname + '/pages/medico.html');
 });
 
 app.listen(port, () => {
